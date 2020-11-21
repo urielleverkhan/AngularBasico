a381805b927a5e0599d885a04745dd797e7460a6
Naira Magalhães <nairadanile@gmail.com>
2020-11-21 17:08:29 -0300

    -Incluso o nav com rotas, incluso rota  e rota baseada em id que recebe, incluso botao edit nos cursos e modo de editar suas informaçoes de error

--- course-manage/src/app/courses/course-list.component.ts
+++ course-manage/src/app/courses/course-list.component.ts
@@ -1,37 +1,32 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './couses.servises';
 
 @Component({
-    selector: 'app-course-list',
-    templateUrl: './course-list-components.html'
+    templateUrl: './course-list.components.html'
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = [];
-    name: string = 'Naira';
+    _courses: Course[] = [];
+    _filterBy: string;
+    filteredCourses: Course[] = [];
+
+    constructor(private courseService: CourseService){ 
+
+    }
 
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: '2, dezembro, 2020'
-            },
-             {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 3,
-                releaseDate: '18, novembro, 2020'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+        
+    }
+
+    set filter(value: string){
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => 
+        course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+    get filter(){
+        return this._filterBy;
     }
 }
